2dee8686704cc1ffc53fae8ed0bd5aa6ca35df6b
Atila Neves <aalvesne@cisco.com>
2013-12-19 16:57:51 +0100

    Moved classes in broker.cpp around to satisfy dependencies

--- src/broker.cpp
+++ src/broker.cpp
@@ -1,75 +1,48 @@
 #include "broker.hpp"
 #include "message.hpp"
 #include <algorithm>
+#include <boost/algorithm/string.hpp>
+#include <unordered_map>
+
+class Subscription {
+public:
+
+    Subscription(MqttSubscriber& subscriber, MqttSubscribe::Topic topic,
+                 std::vector<std::string> topicParts):
+        _subscriber(subscriber),
+        _part(std::move(topicParts[topicParts.size() - 1])),
+        _topic(std::move(topic.topic)),
+        _qos(topic.qos) {
+    }
+
+    void newMessage(std::string topic, std::vector<ubyte> payload) {
+        _subscriber.newMessage(topic, payload);
+    }
+
+    bool isSubscriber(const MqttSubscriber& subscriber) const {
+        return &_subscriber == &subscriber;
+    }
+
+    bool isSubscription(const MqttSubscriber& subscriber,
+                        std::vector<std::string> topics) const {
+        return isSubscriber(subscriber) && isTopic(topics);
+    }
+
+    bool isTopic(std::vector<std::string> topics) const {
+        return std::find(topics.cbegin(), topics.cend(), _topic) == topics.cend();
+    }
+
+private:
+    MqttSubscriber& _subscriber;
+    std::string _part;
+    std::string _topic;
+    ubyte _qos;
+};
 
 
-// struct MqttBroker {
-// public:
 
-//     void subscribe(MqttSubscriber subscriber, in string[] topics) {
-//         subscribe(subscriber, array(map!(a => MqttSubscribe.Topic(a, 0))(topics)));
-//     }
-
-//     void subscribe(MqttSubscriber subscriber, in MqttSubscribe.Topic[] topics) {
-//         foreach(t; topics) {
-//             const parts = array(splitter(t.topic, "/"));
-//             _subscriptions.addSubscription(Subscription(subscriber, t, parts), parts);
-//         }
-//     }
-
-//     void unsubscribe(MqttSubscriber subscriber) {
-//         _subscriptions.removeSubscription(subscriber, _subscriptions._nodes);
-//     }
-
-//     void unsubscribe(MqttSubscriber subscriber, in string[] topics) {
-//         _subscriptions.removeSubscription(subscriber, topics, _subscriptions._nodes);
-//     }
-
-//     void publish(in string topic, in string payload) {
-//         publish(topic, cast(ubyte[])payload);
-//     }
-
-//     void publish(in string topic, in ubyte[] payload) {
-//         auto topParts = array(splitter(topic, "/"));
-//         publish(topic, topParts, payload);
-//     }
-
-//     @property void useCache(bool u) {
-//         _subscriptions._useCache = u;
-//     }
-
-// private:
-
-//     SubscriptionTree _subscriptions;
-
-//     void publish(in string topic, string[] topParts, in ubyte[] payload) {
-//         _subscriptions.publish(topic, topParts, payload);
-//     }
-// };
-
-
-namespace {
-
-bool revStrEquals(const std::string& str1, const std::string& str2) { //compare strings in reverse
-    if(str1.length() != str2.length()) return false;
-    for(int i = str1.length() - 1; i >= 0; --i)
-        if(str1[i] != str2[i]) return false;
-    return true;
-}
-
-bool equalOrPlus(const std::string& pat, const std::string& top) {
-    return pat == "+" || revStrEquals(pat, top);
-}
-
-
-// private struct SubscriptionTree {
-//     private static struct Node {
-//         string part;
-//         Node* parent;
-//         Node*[string] branches;
-//         Subscription[] leaves;
-//     }
-
+struct SubscriptionTree {
+public:
 //     void addSubscription(Subscription s, in string[] parts) {
 //         assert(parts.length);
 //         if(_useCache) _cache = _cache.init; //invalidate cache
@@ -187,48 +160,78 @@ bool equalOrPlus(const std::string& pat, const std::string& top) {
 //         if(_useCache) _cache[topic] ~= sub;
 //     }
 
-
-// private:
-
-//     bool _useCache;
-//     Subscription[] _cache[string];
-//     Node*[string] _nodes;
-// }
-
-
-class Subscription {
-public:
-
-    Subscription(MqttSubscriber& subscriber, MqttSubscribe::Topic topic,
-                 std::vector<std::string> topicParts):
-        _subscriber(subscriber),
-        _part(std::move(topicParts[topicParts.size() - 1])),
-        _topic(std::move(topic.topic)),
-        _qos(topic.qos) {
-    }
-
-    void newMessage(std::string topic, std::vector<ubyte> payload) {
-        _subscriber.newMessage(topic, payload);
-    }
-
-    bool isSubscriber(const MqttSubscriber& subscriber) const {
-        return &_subscriber == &subscriber;
-    }
-
-    bool isSubscription(const MqttSubscriber& subscriber,
-                        std::vector<std::string> topics) const {
-        return isSubscriber(subscriber) && isTopic(topics);
-    }
-
-    bool isTopic(std::vector<std::string> topics) const {
-        return std::find(topics.cbegin(), topics.cend(), _topic) == topics.cend();
-    }
+    void useCache(bool u) { _useCache = u; }
 
 private:
-    MqttSubscriber& _subscriber;
-    std::string _part;
-    std::string _topic;
-    ubyte _qos;
+    struct Node {
+        std::string part;
+        Node* parent;
+        std::unordered_map<std::string, Node*> branches;
+        std::vector<Subscription> leaves;
+    };
+
+    bool _useCache;
+    std::unordered_map<std::string, Subscription*> _cache;
+    std::unordered_map<std::string, Node*> _nodes;
 };
 
-} //anonymous namespace
+
+
+class MqttBroker {
+public:
+
+    void subscribe(MqttSubscriber& subscriber, std::vector<std::string> topics) {
+        std::vector<MqttSubscribe::Topic> newTopics;
+        std::transform(topics.cbegin(), topics.cend(), std::back_inserter(newTopics),
+                       [](std::string t) { return MqttSubscribe::Topic(t, 0); });
+        subscribe(subscriber, newTopics);
+    }
+
+    void subscribe(MqttSubscriber& subscriber, std::vector<MqttSubscribe::Topic> topics) {
+        (void)subscriber;
+        for(const auto& t: topics) {
+            std::vector<std::string> parts;
+            boost::split(parts, t.topic, boost::is_any_of("/"));
+            //_subscriptions.addSubscription(Subscription(subscriber, t, parts), parts);
+        }
+    }
+
+    // void unsubscribe(MqttSubscriber& subscriber) {
+    //     _subscriptions.removeSubscription(subscriber, _subscriptions._nodes);
+    // }
+
+    // void unsubscribe(MqttSubscriber& subscriber, std::vector<std::string> topics) {
+    //     _subscriptions.removeSubscription(subscriber, topics, _subscriptions._nodes);
+    // }
+
+    // void publish(std::string topic, std::vector<ubyte> payload) {
+    //     auto topParts = array(splitter(topic, "/"));
+    //     publish(topic, topParts, payload);
+    // }
+
+    void useCache(bool u) { _subscriptions.useCache(u); }
+
+private:
+
+    SubscriptionTree _subscriptions;
+
+    // void publish(std::string topic, std::vector<std::string> topParts,
+    //              std::vector<ubyte> payload) {
+    //     _subscriptions.publish(topic, topParts, payload);
+    // }
+};
+
+
+namespace {
+
+bool revStrEquals(const std::string& str1, const std::string& str2) { //compare strings in reverse
+    if(str1.length() != str2.length()) return false;
+    for(int i = str1.length() - 1; i >= 0; --i)
+        if(str1[i] != str2[i]) return false;
+    return true;
+}
+
+bool equalOrPlus(const std::string& pat, const std::string& top) {
+    return pat == "+" || revStrEquals(pat, top);
+}
+}
